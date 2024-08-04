233d641e95de4e26b532030f38055a00161e75b5
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-08-04 17:22:25 +0200

    create PageTransition.jsx and install framer-motion

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,8 +1,24 @@
-const PageTransition = () => {
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const PageTransition = ({ children }) => {
+  const pathname = usePathname();
   return (
-    <div>
-      <h1>PageTransition</h1>
-    </div>
+    <AnimatePresence>
+      <div key={pathname}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 0.5, duration: 0.2, ease: "easeInOut" },
+          }}
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        />
+        {children}
+      </div>
+    </AnimatePresence>
   );
 };
 
