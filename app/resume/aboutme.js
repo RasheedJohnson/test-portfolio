f20b57efeb8bf34917017c0440e25a4e69928230
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-08-07 15:37:53 +0200

    add aboutme section to resume dir

---
+++ app/resume/aboutme.js
@@ -0,0 +1,13 @@
+const aboutMeText = {
+  desc: `I'm Rasheed, a software developer from Cape Town, South Africa.
+  
+   My passion for programming began shortly after high school while familiarising myself with microcontrollers and C programming. With an interest in robotics and electronics, I spent much of my time tinkering with arduino boards, from replacing PCBs of wine coolers to building smaller projects.
+   
+   I began learning python in 2018. By 2020, I had developed a keen interest in AI and Computer Science; and started completing various online courses such as IBM's Applied AI program. This interest led to me taking a more serious approach to the learning journey. By that stage I was developing small static sites in HTML, CSS, and JavaScript, including android apps in Java and XML with Firebase as my preferred database at the time.
+   
+   After encountering minor problems, at this point having developed the means to address some of them, I found myself eager to solve whatever I could. In the begining of 2021, during a visit to a doctor's office, I noticed the major inconvenience of dealing with paper records, scanned documents turned to PDFs, duplication of records, and lost files. Here's where I re-evaluated my tech stack with the aim of using combinations of more reliable frameworks and libraries to improve the development experience. I started learning the MERN stack, and quickly moved on to incorporating Next.js into my apps with various libraries and frameworks for animation and styling. I acquired more experience with Tailwind, Node, and MongoDB.
+   
+   After transitioning to the MERN stack, now with additional tools to build larger projects, I shifted my focus with my main ambition being to help solve some problems in healthcare using the skills I've practiced. My largest project still under development attempts to offer solutions to some of those problems experienced by medical professionals and patients. This drive primarily stems from my understanding that these problems are most prevalent in developing countries. Government officials and medical practitioners within these countries are eager to incorporate new and well-developed technologies, provided that they're reliable and affordable. This combination is often hard to find.`,
+};
+
+export default aboutMeText;
--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { Description } from "@radix-ui/react-dialog";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  FaPython,
+} from "react-icons/fa";
+
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiDjango,
+  SiExpress,
+} from "react-icons/si";
+import aboutMeText from "./aboutme";
+
+const about = {
+  title: "About me",
+  description: aboutMeText,
+};
+
 const page = () => {
   return (
     <div>
