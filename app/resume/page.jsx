2eed5b18b9977e4bc2416aab41b5b268fcdd7ed9
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-08-09 15:47:45 +0200

    create setup for work section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Description } from "@radix-ui/react-dialog";
 import {
   FaHtml5,
   FaCss3,
@@ -18,6 +17,7 @@ import {
   SiExpress,
   SiMongodb,
   SiSqlite,
+  SiC,
 } from "react-icons/si";
 import aboutMeText from "./aboutme";
 
@@ -135,6 +135,10 @@ const skills = {
       icon: <FaFigma />,
       name: "figma",
     },
+    {
+      icon: <SiC />,
+      name: "c programming",
+    },
   ],
 };
 
@@ -243,31 +247,35 @@ const page = () => {
               value="about"
               className="w-full text-center xl:text-left"
             >
-              <div className="fex flex-col gap-[30px]">
-                <h3 className="text-4xl font-bold mb-10">{about.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
-                  {about.descriptionOne}
-                </p>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
-                  {about.descriptionTwo}
-                </p>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
-                  {about.descriptionThree}
-                </p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
-                  {about.info.map((item, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className="flex items-center justify-center xl:justify-start gap-4"
-                      >
-                        <span className="text-white/60">{item.fieldName}</span>
-                        <span className="text-xl">{item.fieldValue}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+              <ScrollArea className="h-[730px] shadow-xl shadow-black/20 p-6 rounded-lg">
+                <div className="fex flex-col gap-[30px]">
+                  <h3 className="text-4xl font-bold mb-10">{about.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
+                    {about.descriptionOne}
+                  </p>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
+                    {about.descriptionTwo}
+                  </p>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
+                    {about.descriptionThree}
+                  </p>
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                    {about.info.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-center justify-center xl:justify-start gap-4"
+                        >
+                          <span className="text-white/60">
+                            {item.fieldName}
+                          </span>
+                          <span className="text-xl">{item.fieldValue}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </ScrollArea>
             </TabsContent>
           </div>
         </Tabs>
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,197 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  FaPython,
+} from "react-icons/fa";
+
+import {
+  SiC,
+  SiJavascript,
+  SiMongodb,
+  SiMongoose,
+  SiNextdotjs,
+  SiSqlite,
+  SiTailwindcss,
+} from "react-icons/si";
+
+import Link from "next/link";
+import Image from "next/image";
+import { FiFramer } from "react-icons/fi";
+
+// Projects
+const projects = [
+  {
+    num: "01",
+    category: "Desktop App (Python)",
+    title: "Personal Accounts Manager",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi iste odit dicta magni quae nesciunt nemo officia nihil, adipisci excepturi, esse est. Incidunt id maiores laboriosam aut voluptate magnam voluptatem.",
+    stack: [
+      { name: "Python", icon: <FaPython /> },
+      { name: "SQLite3", icon: <SiSqlite /> },
+      // { name: "CustomTkinter", icon: "./customtkinter.png" },
+    ],
+    image: "",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "full-stack",
+    title: "Psychology Study Base",
+    description:
+      "Magni quae nesciunt nemo officia nihil, adipisci excepturi, esse est. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi iste odit dicta... Incidunt id maiores laboriosam aut voluptate magnam voluptatem.",
+    stack: [
+      { name: "JavaScript", icon: <SiJavascript /> },
+      { name: "MongoDB", icon: <SiMongodb /> },
+      { name: "TailwindCSS", icon: <SiTailwindcss /> },
+      { name: "Next.js", icon: <SiNextdotjs /> },
+      { name: "Node.js", icon: <FaNodeJs /> },
+      { name: "Framer-motion", icon: <FiFramer /> },
+      { name: "Mongoose", icon: <SiMongoose /> },
+    ],
+    image: "",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "full-stack",
+    title: "veterinarian Pet Database",
+    description:
+      "Magni quae nesciunt nemo officia nihil, adipisci excepturi, esse est. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi iste odit dicta... Incidunt id maiores laboriosam aut voluptate magnam voluptatem.",
+    stack: [
+      { name: "JavaScript", icon: <SiJavascript /> },
+      { name: "MongoDB", icon: <SiMongodb /> },
+      { name: "TailwindCSS", icon: <SiTailwindcss /> },
+      { name: "Next.js", icon: <SiNextdotjs /> },
+      { name: "Node.js", icon: <FaNodeJs /> },
+      { name: "Mongoose", icon: <SiMongoose /> },
+    ],
+    image: "",
+    live: "",
+    github: "",
+  },
+  {
+    num: "04",
+    category: "Linux Security (C)",
+    title: "Anonymous Tor Tunnel",
+    description:
+      "Magni quae nesciunt nemo officia nihil, adipisci excepturi, esse est. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi iste odit dicta... Incidunt id maiores laboriosam aut voluptate magnam voluptatem.",
+    stack: [{ name: "C", icon: <SiC /> }],
+    image: "",
+    live: "",
+    github: "",
+  },
+];
+
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
   return (
-    <div>
-      <h1>Work</h1>
-    </div>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px]">
+              {/* Outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+
+              {/* Project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-green-300 transition-all duration-500 capitalize">
+                {project.category}
+              </h2>
+
+              {/* Project description */}
+              <p className="text-white/60">{project.description}</p>
+
+              {/* Stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-green-400">
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="w-14 h-14 bg-[#232329] rounded-xl flex justify-center items-center">
+                            <div className="text-4xl group-hover:text-green-400 transition-all duration-300">
+                              {item.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize text-sm">{item.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  );
+                })}
+              </ul>
+
+              {/* Border */}
+              <div className="border border-white/20"></div>
+              {/* Buttons */}
+              <div className="flex flex-row gap-4">
+                {/* Live Project Button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-green-300" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {/* Github Project Button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center">
+                        <BsGithub className="text-white text-3xl group-hover:text-green-300" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github link</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
   );
 };
 
