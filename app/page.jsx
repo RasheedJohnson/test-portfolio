1cc65e0e960d46af0bb3e753b59317a2d594c2cf
RasheedJohnson <rasheedjohnson@tutanota.com>
2025-10-18 18:27:01 +0200

    add adjusted home page text

--- app/page.jsx
+++ app/page.jsx
@@ -18,13 +18,17 @@ const Home = () => {
             <span className="text-xl">Hi there, I'm Rasheed</span>
             <h1 className="h2 mb-6">
               <span className="text-green-400">
-                A software developer with a keen eye for user experience
+                A Software Engineer driven by Systems Thinking and UX.
               </span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 mx-auto xl:mx-0">
-              After creating software in various languages, using a wide range
-              of technologies over the last 6 years, I've developed a passion
-              for crafting effective digital experiences.
+              My background is unconventional, starting with hardware (C
+              programming for microcontrollers) before transitioning to a
+              seven-year long focus on software engineering. I specialise in the
+              Next.js ecosystem, motivated to create robust and effective
+              digital experiences. My current ambition includes solving systemic
+              inefficiencies in healthcare within developing countries like
+              South Africa.
             </p>
 
             {/* Buttons and Socials */}
